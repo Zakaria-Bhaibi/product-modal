fd615151d46f162200d5ad981df58275666867b3
Zakaria Bhaibi <bhaibizakaria@gmail.com>
2023-06-02 21:21:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
               {/* Buttom buttons container */}
               <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-4 md:flex-row">
                 <button className="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150">
-                  <img src="src\assets\cart.png" alt="" className="w-8" />
+                  <img src="src/assets/cart.png" alt="" className="w-8" />
                   <span>ADD TO CART</span>
                 </button>
                 <button className="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150">
